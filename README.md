82f995c69e7c261c48a7681e9180751bf55271bf
Yanghangfeng <1437183653@qq.com>
2016-12-03 20:33:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,76 @@
+## 把网页内容或文本文件生成特定的云标签
+```
+                      $$\                         
+                      $$ |                        
+ $$$$$$\   $$$$$$$\ $$$$$$\    $$$$$$\   $$$$$$\  
+$$  __$$\ $$  _____|\_$$  _|   \____$$\ $$  __$$\
+$$ /  $$ |$$ /        $$ |     $$$$$$$ |$$ /  $$ |
+$$ |  $$ |$$ |        $$ |$$\ $$  __$$ |$$ |  $$ |
+\$$$$$$$ |\$$$$$$$\   \$$$$  |\$$$$$$$ |\$$$$$$$ |
+ \____$$ | \_______|   \____/  \_______| \____$$ |
+$$\   $$ |                              $$\   $$ |
+\$$$$$$  |                              \$$$$$$  |
+ \______/                                \______/
+                            --------- by Yanghangfeng
+```
+
+#### 1.把仓库克隆到本地
+```
+>>> git clone
+```
+#### 2.安装依赖文件
+* 方法一
+```
+>>> pip install -r requirements.txt
+```
+* 方法二(推荐)
+如果方法一安装失败，可以在这个[网站下载](http://www.lfd.uci.edu/~gohlke/pythonlibs/)相应的.whl包,然后pip安装即可
+```
+>>> pip install wheel
+```
+##### 请注意！！！一定要在.whl文件目录里打开命令行窗口
+```
+>>> pip install 下载文件名.whl
+```
+#### 3.参数介绍
+- `--url`:待画网页的url， 默认为`None`
+- `--input`:本地待画的文本文件路径
+- `--output`:生成的图片保存形式(可选参数，默认为运行文件的根目录且为wordcloud.jpg)
+- `--model`:生成图片的形式(可选参数，自定义图片mask)
+- `--ttf`:标签云中的字体格式(可选参数)
+- `--width`:生成图片的宽度(可选参数，默认为1800)
+- `--height`:生成图片的高度(可选参数，默认为1000)
+- `--bg`:生成图片的背景颜色(可选参数，black or white，默认为black)
+- `--margi`:生成图片中文字的边距(可选参数，默认为5)
+说明: `--url`,`--input`参数必选其一
+#### 4.使用方法
+##### 4-1. 在线获取网页并生成标签云
+```
+>>> python gctag.py --url https://www.douban.com/note/591278881/ --output ./douban.jpg --bg white --ttf ./font/叶立群几何体.ttf
+```
+* 结果一:
+![]()
+
+```
+>>> python gctag.py --url https://www.douban.com/note/591278881/ --output ./douban_your_name2.jpg --bg white --ttf ./font/奇思_奔跑吧电影.ttf
+```
+* 结果二:
+![]()
+
+##### 4-2.利用本地文件生成标签云
+```
+>>> python gctag.py --input ./test.txt --output ./js.jpg --bg white --ttf ./font/李旭科漫画体v1.0.ttf
+```
+
+* 结果三:
+![]()
+
+以alice_mask.png为模板生成标签云
+```
+>>> python gctag.py --input ./alice.txt --model ./alice_mask.png --bg white --output ./alice.jpg
+```
+![](alice模板)
+* 结果四:
+![]()
+
+#### 5.
---
+++ gctag.py
@@ -0,0 +1,93 @@
+import re
+import sys
+import argparse
+from os import path
+
+import PIL
+import jieba
+import requests
+import numpy as np
+from lxml import html
+from wordcloud import WordCloud
+
+
+def read_stopwords(file_path):
+    stopwords = set()
+    with open(file_path, 'rb') as fp:
+        for line in fp.readlines():
+            if not line.strip():
+                continue
+            stopwords.add(line.strip().decode('utf-8'))
+
+    return stopwords
+
+def get_html_text(url):
+    response = requests.get(url)
+    origin_text = response.text
+    origin_text = re.sub(r'<script.*?>.*?</script>', '', origin_text, flags=re.I | re.M | re.DOTALL)
+    origin_text = re.sub(r'<style.*?>.*?</style>', '', origin_text, flags=re.I | re.M | re.DOTALL)
+
+    doc = html.fromstring(origin_text)
+    text = doc.xpath('//body//text()')
+    text = [i.strip() for i in text if i.strip()]
+    text = ' '.join(text)
+    seg = jieba.cut(text)
+
+    stopwords = read_stopwords('./utils/stopwords.txt') # callable read_stopwords()
+    seg = [i.strip() for i in seg if i.strip() and not i.strip().isdigit()
+           and i.strip() not in stopwords]
+    seg = ' '.join(seg)
+
+    return seg
+
+def get_txt(txt_file_path):
+    with open(txt_file_path, 'r') as fp:
+        txt_data = fp.read()
+    words = list(jieba.cut(txt_data))
+    words_list = [word for word in words if len(word)>1]
+    word_join = ' '.join(words_list)
+
+    return word_join
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', metavar='URL', default=None, help='input the url')
+    parser.add_argument('--output', metavar='OUTPUT', default='./wordcloud.jpg', help='input the output_file')
+    parser.add_argument('--input', metavar='INPUT_FIEL', default=None, help='input the input_file')
+    parser.add_argument('--model', metavar='INPUT_IMAGE_MODEL', default=None, help='input the input_image_model')
+    parser.add_argument('--ttf', metavar='INPUT_TTF', default='./font/simhei.ttf', help='input the typeface')
+    parser.add_argument('--width', metavar='INPUT_WIDTH', default=1800, type=int, help='input the image width')
+    parser.add_argument('--height', metavar='INPUT_HEIGHT', default=1000, type=int, help='input the image height')
+    parser.add_argument('--bg', metavar='INPUT_BACKGROUND_COLOR', default='black', help='input the image background_color')
+    parser.add_argument('--margin', metavar='INPUT_MARGIN', default=5, type=int, help='input the image margin')
+    args = parser.parse_args()
+
+    url = args.url
+    output_file = args.output
+    input_file = args.input
+    model_path = args.model
+    typeface = args.ttf
+    width = args.width
+    height = args.height
+    background_color = args.bg
+    margin = args.margin
+
+    try:
+        image_mask = np.array(PIL.Image.open(model_path))
+    except:
+        image_mask=None
+
+    wordcloud = WordCloud(font_path=typeface, mask=image_mask,
+                          background_color=background_color, margin=margin, width=width, height=height)
+    try:
+        txt_join = get_txt(input_file)
+        wordcloud_ = wordcloud.generate(txt_join)
+    except:
+        html_text = get_html_text(url)
+        wordcloud_ = wordcloud.generate(html_text)
+
+    image = wordcloud_.to_image()
+    image.save(output_file)
+
+if __name__ == '__main__':
+    main()
