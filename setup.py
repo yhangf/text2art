27ce98d1506c9e7074b222e335c683ab3e06e3cf
JagTheFriend <jagthefriend12@gmail.com>
2021-04-06 17:43:59 +0200

    Formatted file

--- setup.py
+++ setup.py
@@ -6,25 +6,25 @@ with open("README.md", "r", encoding="utf-8") as fp:
     long_description = fp.read()
 
 setup(
-    name = "text2art",
-    version = __version__,
-    description = "Generating controlled terminal art fonts",
+    name="text2art",
+    version=__version__,
+    description="Generating controlled terminal art fonts",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license = "MIT",
-    include_package_data = True,
-    author = "hangfengYang",
-    author_email = "yhf5fhy@gmail.com",
-    classifiers = [],
-    url = "https://github.com/yhangf/text2art",
-    packages = ["text2art"],
-    install_requires = [
+    license="MIT",
+    include_package_data=True,
+    author="hangfengYang",
+    author_email="yhf5fhy@gmail.com",
+    classifiers=[],
+    url="https://github.com/yhangf/text2art",
+    packages=["text2art"],
+    install_requires=[
         "fire",
         "colorama",
         "pyfiglet"
     ],
-    
-    entry_points = {
+
+    entry_points={
         "console_scripts": [
             "text2art  =  text2art.main:main",
         ],
